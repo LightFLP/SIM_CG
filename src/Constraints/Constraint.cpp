c9e93db1e9f0f274d76f94033176ce59c091bc0b
Supwatch <github@bvanleeuwen.com>
2022-05-17 13:01:56 +0200

    Add Constraint destructor

--- src/Constraints/Constraint.cpp
+++ src/Constraints/Constraint.cpp
@@ -6,3 +6,11 @@ void Constraint::addConstraint(Constraint* constraint) {
     constraint->m_c_index = Constraint::_constraints.size();
     Constraint::_constraints.push_back(constraint);
 };
+
+Constraint::~Constraint() {
+    for (Constraint *c : Constraint::_constraints) {
+        c->iVector.clear();
+        delete c;
+    }
+    Constraint::_constraints.clear();
+}
