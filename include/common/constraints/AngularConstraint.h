f679efc5c0378b1d5e3b3799c3233b8a096c9631
Filip Zamfirov <filip.zamfirov1996@gmail.com>
2022-05-25 13:42:31 +0200

    Angular constraint files

--- include/common/constraints/AngularConstraint.h
+++ include/common/constraints/AngularConstraint.h
@@ -1,8 +1,20 @@
-//
-// Created by fzamf on 2022-05-22.
-//
+#pragma once
 
-#ifndef SIM_CG_ANGULARCONSTRAINT_H
-#define SIM_CG_ANGULARCONSTRAINT_H
+#include <gfx/vec2.h>
 
-#endif //SIM_CG_ANGULARCONSTRAINT_H
+#include "Constraint.h"
+
+class AngularConstraint : public Constraint {
+    double const m_ra;
+    Vec2 p0, p1, p2;
+
+public:
+    AngularConstraint(int p_index1, int p_index2, int p_index3, double ra);
+
+    virtual void draw();
+
+    virtual double eval_C(GlobalVars *globals);
+    virtual double eval_Cdot(GlobalVars *globals);
+    virtual void eval_J(GlobalVars *globals, std::vector <MatrixBlock> &blocks);
+    virtual void eval_Jdot(GlobalVars *globals, std::vector <MatrixBlock> &blocks);
+};
